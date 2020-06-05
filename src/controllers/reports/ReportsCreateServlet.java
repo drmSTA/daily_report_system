57e73588330f5100771e0b24f5d6007be3208480
drmS <tnstarktig@gmail.com>
2020-06-05 22:20:06 +0900

    Review ReportsCreateServlet

--- src/controllers/reports/ReportsCreateServlet.java
+++ src/controllers/reports/ReportsCreateServlet.java
@@ -22,7 +22,7 @@ import utility.WB;
  */
 @WebServlet(WB.PATH_REPORT_CREATE)
 public class ReportsCreateServlet extends HttpServlet {
-    private static final long serialVersionUID = 20200601L;
+    private static final long serialVersionUID = 20200605L;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -38,26 +38,31 @@ public class ReportsCreateServlet extends HttpServlet {
       String previousToken = (String)request.getParameter(WB.K_TOKEN);
       String currentToken = request.getSession().getId();
 
+      //CSRF対策
       if(StringValidator.isUnderValidSession(previousToken, currentToken)) {
+        // 前ページ より 入力内容 を取得、Report インスタンスの作成
         Employee employee = (Employee)request.getSession().getAttribute(WB.K_LOGIN_EMPLOYEE);
         String _reportDate = request.getParameter(WB.K_REPORT_DATE);
         Date reportDate = StringValidator.isEmpty(_reportDate) ? new Date(System.currentTimeMillis()) : Date.valueOf(_reportDate);
         String title = request.getParameter(WB.K_REPORT_TITLE);
         String contents = request.getParameter(WB.K_REPORT_CONTENTS);
-
         Report report = new Report(employee, reportDate, title, contents);
+
+        // validation を行う
         List<String> errorMessages = report.performValidation4NewRegistration();
 
         if(errorMessages.size() > 0) {
-          // if there is error(s) found, back to the page
+          // 入力内容に不具合があるため errorMessages とともに new.jsp へ転送
           request.setAttribute(WB.K_TOKEN, currentToken);
           request.setAttribute(WB.K_REPORT, report);
           request.setAttribute(WB.K_ERRORS, errorMessages);
 
           request.getRequestDispatcher(WB.PATH_REPORT_NEW_JSP).forward(request, response);
         } else {
-          //
+          // DBへ登録
           DBHandler.putANewReport2DB(report);
+
+          // flush を設定し、index ページへリダイレクト
           request.getSession().setAttribute(WB.K_FLUSH, "登録が完了しました。");
           response.sendRedirect(request.getContextPath() + WB.PATH_REPORT_INDEX);
         }
