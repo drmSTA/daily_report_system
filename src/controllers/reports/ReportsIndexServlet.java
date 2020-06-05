8b732029c0f8fceb3942e1bd01457b08e0edae29
drmS <tnstarktig@gmail.com>
2020-06-05 22:21:32 +0900

    Review ReportsIndexServlet

--- src/controllers/reports/ReportsIndexServlet.java
+++ src/controllers/reports/ReportsIndexServlet.java
@@ -3,17 +3,13 @@ package controllers.reports;
 import java.io.IOException;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Employee;
 import models.Report;
-import utility.DBConnector;
 import utility.DBHandler;
 import utility.FlushHandler;
 import utility.WB;
@@ -23,7 +19,7 @@ import utility.WB;
  */
 @WebServlet(WB.PATH_REPORT_INDEX)
 public class ReportsIndexServlet extends HttpServlet {
-    private static final long serialVersionUID = 20200601L;
+    private static final long serialVersionUID = 20200605L;
 
     /**
      * @see HttpServlet#HttpServlet()
