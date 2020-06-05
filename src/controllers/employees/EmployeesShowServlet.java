c003c47823bb3f8584f32442c00480c61fef918b
drmS <tnstarktig@gmail.com>
2020-06-05 22:08:00 +0900

    Review EmployeesShowServlet

--- src/controllers/employees/EmployeesShowServlet.java
+++ src/controllers/employees/EmployeesShowServlet.java
@@ -1,8 +1,6 @@
 package controllers.employees;
 import java.io.IOException;
 
-import javax.persistence.EntityManager;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Employee;
-import utility.DBConnector;
 import utility.DBHandler;
 import utility.WB;
 
@@ -19,7 +16,7 @@ import utility.WB;
  */
 @WebServlet(WB.PATH_EMPLOYEE_SHOW)
 public class EmployeesShowServlet extends HttpServlet {
-    private static final long serialVersionUID = 20200601L;
+    private static final long serialVersionUID = 20200605L;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -32,6 +29,7 @@ public class EmployeesShowServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      //セッションスコープから employee id を取得、セッションスコープにDBより取得した employee インスタンスを設定
       int employeeID = Integer.parseInt(request.getParameter(WB.K_EMPLOYEE_ID));
       Employee employee = DBHandler.getEmployee(employeeID);
       request.setAttribute(WB.K_EMPLOYEE, employee);
