1e8f17fec78c5c794750213ab36f8995d0ded1ff
drmS <tnstarktig@gmail.com>
2020-06-05 21:59:35 +0900

    Review EmployeesEditServlet

--- src/controllers/employees/EmployeesEditServlet.java
+++ src/controllers/employees/EmployeesEditServlet.java
@@ -2,8 +2,6 @@ package controllers.employees;
 
 import java.io.IOException;
 
-import javax.persistence.EntityManager;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,9 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Employee;
-import utility.DBConnector;
 import utility.DBHandler;
-import utility.Dumper;
 import utility.WB;
 
 /**
@@ -21,7 +17,7 @@ import utility.WB;
  */
 @WebServlet(WB.PATH_EMPLOYEE_EDIT)
 public class EmployeesEditServlet extends HttpServlet {
-    private static final long serialVersionUID = 20200601L;
+    private static final long serialVersionUID = 20200605L;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -34,17 +30,17 @@ public class EmployeesEditServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // id 処理。前ページから入力がなければデフォルト(id 0 = DB上にデータなし)
       int id = 0;
       try{
           id = Integer.parseInt(request.getParameter(WB.K_EMPLOYEE_ID));
       } catch(NumberFormatException e) { }
       request.getSession().setAttribute(WB.K_EMPLOYEE_ID, id);
 
+      // DB から該当のEmployeeを取得（null許容）、HttpServletRequest に設定、ページ転送
       Employee employee = DBHandler.getEmployee(id);
       request.setAttribute(WB.K_EMPLOYEE, employee);
-
       request.setAttribute(WB.K_TOKEN, request.getSession().getId());
-
       request.getRequestDispatcher(WB.PATH_EMPLOYEE_EDIT_JSP).forward(request, response);
     }
 
